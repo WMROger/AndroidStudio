d5d56778cd02e63bcfc57d95576043abf8aa8bce
WMROger <63821425+WMROger@users.noreply.github.com>
2024-09-08 12:51:05 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/navigationdrawer/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.navigationdrawer;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Start MainActivity after a delay (e.g., 2 seconds)
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();  // Finish SplashActivity so it doesn't appear on back stack
+            }
+        }, 2000); // 2000ms delay (2 seconds)
+    }
+}
