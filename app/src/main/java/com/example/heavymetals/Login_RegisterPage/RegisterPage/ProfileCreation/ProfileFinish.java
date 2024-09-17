5e88056220e905da967dadab0efe4926b37744e7
WMROger <63821425+wmroger@users.noreply.github.com>
2024-09-17 13:34:28 +0800

    STARTING NEW ERA

--- app/src/main/java/com/example/heavymetals/Login_RegisterPage/RegisterPage/ProfileCreation/ProfileFinish.java
+++ app/src/main/java/com/example/heavymetals/Login_RegisterPage/RegisterPage/ProfileCreation/ProfileFinish.java
@@ -2,35 +2,37 @@ package com.example.heavymetals.Login_RegisterPage.RegisterPage.ProfileCreation;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.heavymetals.Login_RegisterPage.LoginPage.LoginActivity;
 import com.example.heavymetals.R;
 
-public class FitnessDeclaration extends AppCompatActivity {
-    Button btnPFDnext;
-
+public class ProfileFinish extends AppCompatActivity {
+    private Button btnDone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_profile_fitness_declaration);
+        setContentView(R.layout.activity_profile_finish);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        btnPFDnext = findViewById(R.id.btnPFDnext1);
-
-        btnPFDnext.setOnClickListener(v -> {
-            Intent intent = new Intent(FitnessDeclaration.this, FitnessDeclaration2.class);
-            startActivity(intent);
+        btnDone = findViewById(R.id.btnDone);
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileFinish.this, LoginActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
