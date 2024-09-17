4d170fab4f46051bd7045ca7f74b4b2f983fdc72
WMROger <63821425+wmroger@users.noreply.github.com>
2024-09-17 13:34:28 +0800

    FINAL NA SA LOGIN/REGISTER DONT TOUCH

--- app/src/main/java/com/example/heavymetals/Login_RegisterPage/RegisterPage/ProfileCreation/FitnessDeclaration3.java
+++ app/src/main/java/com/example/heavymetals/Login_RegisterPage/RegisterPage/ProfileCreation/FitnessDeclaration3.java
@@ -1,29 +1,32 @@
 package com.example.heavymetals.Login_RegisterPage.RegisterPage.ProfileCreation;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.example.heavymetals.Home_LandingPage.MainActivity;
 import com.example.heavymetals.Login_RegisterPage.LoginActivity;
 import com.example.heavymetals.R;
 
 public class FitnessDeclaration3 extends AppCompatActivity {
-
+    TextView Fitness_Declaration_2;
     Button btnPFDnext3;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fitness_declaration3);
-
+        setContentView(R.layout.activity_profile_fitness_declaration_3);
+        Fitness_Declaration_2 = findViewById(R.id.Fitness_Declaration_2);
         btnPFDnext3 = findViewById(R.id.btnPFDnext3);
 
+        Fitness_Declaration_2.setOnClickListener(v -> {
+            Intent intent = new Intent(FitnessDeclaration3.this, FitnessDeclaration2.class);
+            startActivity(intent);
+        });
         btnPFDnext3.setOnClickListener(v -> {
             Intent intent = new Intent(FitnessDeclaration3.this, LoginActivity.class);
             startActivity(intent);
