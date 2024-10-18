0afd8ba730d6dbf578db1600fce631a057eec007
WMROger <63821425+WMROger@users.noreply.github.com>
2024-10-18 12:09:41 +0800

    COMPLETO NA TANAN HAHAHAHA

--- app/src/main/java/com/example/heavymetals/Home_LandingPage/Workouts/WorkoutDetailActivity.java
+++ app/src/main/java/com/example/heavymetals/Home_LandingPage/Workouts/WorkoutDetailActivity.java
@@ -144,7 +144,20 @@ public class WorkoutDetailActivity extends AppCompatActivity {
 
         exercisesContainer.addView(exerciseTextView);
 
+        CheckBox doneCheckbox = new CheckBox(this);
+        doneCheckbox.setChecked(adaptersExercise.isDone());
+        doneCheckbox.setText("Completed");
+        doneCheckbox.setButtonTintList(getResources().getColorStateList(R.color.white));
+        doneCheckbox.setTextColor(getResources().getColor(R.color.white));
 
+        LinearLayout.LayoutParams checkboxParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        checkboxParams.setMargins(0, 8, 0, 55); // Adding margins below the checkbox
+        doneCheckbox.setLayoutParams(checkboxParams);
+
+        doneCheckbox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            adaptersExercise.setDone(isChecked);  // Update the exercise "isDone" status
+        });
 
         LinearLayout.LayoutParams checkboxParams = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
